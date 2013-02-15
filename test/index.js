ded35b06394db3a25c6d5b03f36fc84f47cf7317
Mirko Kiefer <mail@mirkokiefer.com>
2013-02-16 00:30:15 +0100

    add connect middleware

--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
 
 var assert = require('assert')
 var cache = require('./test-cache-config')
-var geocode = require('../lib/index')({cache: cache, init: true})
-//var geocode = require('../lib/index')()
+var opts = {cache: cache, init: true}
+var geocode = require('../lib/index')(opts)
+var req = require('superagent')
 
 var places = ['New York', 'Heidelberg', 'Tokio', 'Cape Town']
+
+after(function(done) { cache.destroy(done) })
 
 describe('geocode', function() {
   it('should batch lookup multiple locations', function(done) {
@@ -21,7 +24,27 @@ describe('geocode', function() {
       res.forEach(function(each, i) {
         assert.equal(each.provided, newPlaces[i])
       })
-      cache.destroy(done)
+      done()
     })
+  })
+})
+describe('geocode middleware', function() {
+  var express = require('express')
+  var middleware = require('../lib/index').middleware({cache: cache, init: true})
+  var app = express()
+  app.use(express.bodyParser())
+  app.use(middleware)
+  app.listen(3000)
+
+  it('should test the middleware', function(done) {
+    req
+      .post('localhost:3000/geocode')
+      .send({places: places})
+      .end(function(res) {
+        res.body.places.forEach(function(each, i) {
+          assert.equal(each.provided, places[i])
+        })
+        done()
+      })
   })
 })
